48bad5ef8050e9f581bc92fc2c450a0735616c8f
Olivier <olivier.winkler@icloud.com>
2024-08-27 10:26:25 +0200

    feat(app): update package.json

--- packages/ui/src/Button.tsx
+++ packages/ui/src/Button.tsx
@@ -1,8 +1,6 @@
-import * as React from 'react';
-
 export const Button = () => {
   return (
-    <div className="rounded-md ">
+    <div className="rounded-md">
       <a href="https://turbo.build/repo/docs">
         <div className="flex w-full items-center justify-center rounded-md border border-transparent bg-white px-8 py-3 text-base font-medium text-black no-underline hover:bg-gray-300 md:px-10 md:py-3 md:text-lg md:leading-6">
           Read the docs
