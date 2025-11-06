31e7efaf1a8b04ab954b4cce7efb1a23e4eda8e2
Olivier <olivier.winkler@icloud.com>
2025-11-06 13:13:45 +0100

    fix: correct order of padding classes in main element

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -26,7 +26,7 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <main className="mx-auto w-auto px-4 pb-8 pt-16 sm:pt-24 lg:px-8">
+      <main className="mx-auto w-auto px-4 pt-16 pb-8 sm:pt-24 lg:px-8">
         <h1 className="mx-auto text-center text-6xl font-extrabold tracking-tight text-white sm:text-7xl lg:text-8xl xl:text-8xl">
           Web
           <span className="from-brandred to-brandblue block bg-gradient-to-r bg-clip-text px-2 text-transparent">
